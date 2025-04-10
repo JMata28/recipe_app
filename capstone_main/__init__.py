188b5aecad14b1f33d0ba9245fbce16f7db68795
José Mata <josemata@Joses-MacBook-Pro.local>
2025-04-09 23:15:18 -0400

    user authentication implemented and tested

--- capstone_main/__init__.py
+++ capstone_main/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from dotenv import load_dotenv
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 #load variables from .env
@@ -15,5 +17,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
 app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page' #to deal with the @login_required decorator
+login_manager.login_message_category = 'info'
 
 from capstone_main import routes
