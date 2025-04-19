4c595f1c55298cddca63b7ccb67e6a71126f1066
José Mata <josemata@Joses-MacBook-Pro.local>
2025-04-19 15:15:03 -0400

    full implementation of Account page features - changing account details and profile picture

--- capstone_main/routes.py
+++ capstone_main/routes.py
@@ -1,7 +1,10 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from capstone_main import app, db, bcrypt
 from capstone_main.models import User, Recipe
-from capstone_main.forms import RegistrationForm, LoginForm
+from capstone_main.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from capstone_main.private_variables import *
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -48,7 +51,34 @@ def logout_page():
     logout_user()
     return redirect(url_for('home_page'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static', picture_fn)
+
+    output_size = (125, 125) 
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account", methods = ['GET', 'POST'])
 @login_required
 def account_page():
-    return render_template('account.html', title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data: 
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file =  picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account_page')) #causes the browser to send a GET request to avoide the POST GET REDIRECT PATTERN
+    elif request.method == 'GET':
+        form.username.data = current_user.username #pre-fills out the username in the form
+        form.email.data = current_user.email #pre-fills out the email in the form
+    image_file =  url_for('static', filename=current_user.image_file)
+    return render_template('account.html', title="Account", image_file=image_file, form=form)
